d5abca52eb6a0b69b834fae0c505ea4dd6ce128d
Mickdevv <michael@devereux.fm>
2023-12-05 22:05:31 +0100

    react-redux added. Not currently working : State management dependencies are conflicting. Fix needed

--- backend/base/views.py
+++ backend/base/views.py
@@ -11,18 +11,18 @@ from .serializers import	ProductSerializer
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        '/api/products/'
-        '/api/products/create/'
+        '/api/products/',
+        '/api/products/create/',
         
-        '/api/products/upload/'
+        '/api/products/upload/',
         
-        '/api/products/<id>/reviews/'
+        '/api/products/<id>/reviews/',
         
-        '/api/products/top/'
-        '/api/products/<id>/'
+        '/api/products/top/',
+        '/api/products/<id>/',
         
-        '/api/products/delete/<id>/'
-        '/api/products/<update>/<id>/'
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
         
     ]
     return Response(routes)
